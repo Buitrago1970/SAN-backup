cf17aae3ad7c7f6201c0cedfb05b6496ac87da7f
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-10-19 15:46:23 -0500

    cambios

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,84 +1,30 @@
-// import React, { useState, useContext, useEffect } from "react";
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 
-//components
-// import ProductItem from "./ProductItem";
-// import Categories from "./Categories";
-// import Carousel from "./Carousel";
-// import PageLoanding from "./PageLoanding";
-// import Appcontext from "../context/Appcontext";
+// components
+import ProductItem from "./ProductItem";
+import Categories from "./Categories";
+import Carousel from "./Carousel";
+import PageLoanding from "./PageLoanding";
+import Appcontext from "../context/Appcontext";
 
-//styles
-// import "./styles/productList.css";
-// import "./styles/navbar.css";
-// import { Link } from "react-router-dom";
-// import { FiShoppingCart } from "react-icons/fi";
-// import { AiOutlineSearch } from "react-icons/ai";
-import under_Construction from "../images/SAN Under_Construction(Black).png";
+// styles
+import "./styles/productList.css";
+import "./styles/navbar.css";
 
 function ProductsList() {
-  // const initialState = {
-  //   res: [
-  //     {
-  //       id: "01",
-  //       name: "Lomo Limpio",
-  //       price: "38.000",
-  //       measues: "kilogramo",
-  //       amount: "500g",
-  //       presentation: "Filete",
-  //       img: "http://dummyimage.com/800x600.png/99118E/ffffff",
-  //     },
-  //     {
-  //       id: "03",
-  //       name: "Chatas En Posta",
-  //       price: "24.000",
-  //       uniti: "kilogramo",
-  //       amount: "500g",
-  //       presentation: "Filete",
-  //       img: "http://dummyimage.com/800x600.png/99118E/ffffff",
-  //     },
-  //   ],
-  // };
-  // const item = useContext(Appcontext);
-  // const RenderList = (item = []) => {
-  //   return (
-  //     <>
-  //       {item.map((items) => (
-  //         <ProductItem key={items.id} {...items} />
-  //       ))}
-  //     </>
-  //   );
-  // };
-  // if (loanding) {
-  //   return <PageLoanding />;
-  // }
+  const item = useContext(Appcontext);
+  const RenderList = (item = []) => {
+    return (
+      <>
+        {item.map((items) => (
+          <ProductItem key={items.id} {...items} />
+        ))}
+      </>
+    );
+  };
+
   return (
     <>
-      <div className="containerImg">
-        <img src={under_Construction} alt="" />
-      </div>
-      {/* <nav>
-        <div className="search">
-          <div className="container-serch">
-            <span className="icon">
-              <i className="fa fa-search">
-                <AiOutlineSearch />
-              </i>
-            </span>
-            <input
-              onChange={(value) => {}}
-              type="text"
-              id="search"
-              placeholder="Buscar..."
-            />
-          </div>
-        </div>
-        <div className="container-shopping-cart-nav">
-          <Link to="/carroCompras">
-            <FiShoppingCart className="shopping-cart-nav" />
-          </Link>
-        </div>
-      </nav>
       <h1 className="principal-title">Carniceria Online</h1>
       <h3 className="title-secondary">surticarnes san diego</h3>
       <div className="list-container">
@@ -97,7 +43,7 @@ function ProductsList() {
             <Carousel>{RenderList(item.chicken)}</Carousel>
           </Categories>
         )}
-      </div> */}
+      </div>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-// import page home styles
+//styles
 import "./styles/home.css";
 
-import ProductsList from "../components/ProductsList.jsx";
 import Navbar from "../components/Navbar";
+import ProductsList from "../components/ProductsList.jsx";
 
 class Home extends React.Component {
   render() {
     return (
       <>
-        {/* <Navbar />
-        <h1 className="principal-title">Carniceria Online</h1>
+        <Navbar />
+        {/* <h1 className="principal-title">Carniceria Online</h1>
         <h3 className="title-secondary">surticarnes san diego</h3> */}
         <ProductsList />
       </>
