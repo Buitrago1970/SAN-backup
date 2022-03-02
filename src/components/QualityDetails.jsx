83010dd95a1ea1e51a60212913760b64df144f5c
Juan Buitrago <juanbuitrago2002@outlook.com>
2022-03-02 12:02:20 -0500

    estilos mobile servicios

--- src/components/QualityDetails.jsx
+++ src/components/QualityDetails.jsx
@@ -1,46 +1,33 @@
 import React from "react";
 
 import "./styles/QualityDetails.css";
-import crow from "../images/crow.png";
+import image1993 from "../images/1993.png"
+import image500000 from "../images/500.000.png"
+import image300 from "../images/300.png"
 
 function QualityDetails() {
-  const initialState = {
-    badges: [
-      {
-        number: "1993",
-        title: "Desde el año 1993",
-      },
-      {
-        number: "+ 500.000",
-        title: "+ de 500.000 pedidos",
-      },
-      {
-        number: "+ 300",
-        title: "+ de 300 productos",
-      },
-    ],
-  };
-  const { badges } = initialState;
+
+  const qualityDetails = [
+    {
+      image: image1993,
+    },
+    {
+      image: image500000,
+    },
+    {
+      image: image300,
+    },
+  ];
   return (
     <>
       <section className="section-badges">
-        <div className="container-badges">
           <p className="sub-title-quality-details">Logros</p>
-          <div className="badges">
-            {badges.map((item) => (
-              <>
-                <div className="badges-item" >
-                  <img className="imgs" src={crow} alt="" />
-                  <div>
-                  <p className="number-badge">{item.number}</p>
-                  <p className="p-quality-details">{item.title}</p>
-                  </div>
-            
-                  <img className="img-reverse" src={crow} alt="" />
-                </div>
-              </>
-            ))}
-          </div>
+        <div className="badges-container">
+          {qualityDetails.map((qualityDetail, index) => (
+             <div className="badges" key={index}>
+            <img src={qualityDetail.image} alt="" />
+          </div>))
+            }
         </div>
       </section>
     </>
