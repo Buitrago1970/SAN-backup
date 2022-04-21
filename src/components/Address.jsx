3698af7d5c56f1c2b1e9788fef171e150dd74a16
Juan Buitrago <juanbuitrago2002@outlook.com>
2022-04-21 12:10:30 -0500

    x

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -7,32 +7,30 @@ import Appcontext from "../context/Appcontext";
 import "./styles/Address.css";
 
 export default function Address({user, cart }) {
-  const {state:{address_info}} = useContext(Appcontext)
-  if(address_info.length > 0){
-    return ( <>
-      {
-         user &&  cart.length > 0 ? <div className="container-address">
-         <div className="title-container">
-           <div className="secondary-title">Opciones de envío a</div>
-         </div>
-         <div className="address-contrainer-info">
-           <div className="address">
-         <GoLocation />
-             <div>
-                <p className="cart-address-info">{user.direccional} </p>
-               <p className="cart-address-subtitle-info"> {user.localidad}, Bogota D.c - {user.descripcionCasa && user.descripcionCasa}</p>
-           </div>
-           <div className="modify-address">
-           </div>
-         </div>
-         {user ? <Link to="/carrocompras/{}/direccion" className="modify">Modificar dirección</Link>: <Link to="/carrocompras/{}/direccion" className="modify">Agregar dirección</Link>}
-   
-       </div> 
-       </div> : 
-       null
-       }
-       </>)
-  }else{
-    return null
-  }
+  console.log(user);
+  return (
+    <>
+   {
+      user &&  cart.length > 0 ? <div className="container-address">
+      <div className="title-container">
+        <div className="secondary-title">Opciones de envío a</div>
+      </div>
+      <div className="address-contrainer-info">
+        <div className="address">
+      <GoLocation />
+          <div>
+             <p className="cart-address-info">{user.direccional} </p>
+            <p className="cart-address-subtitle-info"> {user.localidad}, Bogota D.c - {user.descripcionCasa && user.descripcionCasa}</p>
+        </div>
+        <div className="modify-address">
+        </div>
+      </div>
+      {user ? <Link to="/carrocompras/{}/direccion" className="modify">Modificar dirección</Link>: <Link to="/carrocompras/{}/direccion" className="modify">Agregar dirección</Link>}
+
+    </div> 
+    </div> : 
+    null
+    }
+    </>
+  );
 }
